3b1eff67745f7d9046538c54ef2560a266af1e98
chaynadas <116340466+chaynadas@users.noreply.github.com>
2023-03-13 17:08:42 +0530

    Add files via upload

--- game_page.js
+++ game_page.js
@@ -10,8 +10,8 @@ document.getElementById("player2_name").innerHTML = player2_name + " :";
 document.getElementById("player1_score").innerHTML = player1_score;
 document.getElementById("player2_score").innerHTML = player2_score;
 
-document.getElementById("player_question").innerHTML = "Question Turn - " + player1_name;
-document.getElementById("player_answer").innerHTML = "Answer Turn - " + player2_name;
+document.getElementById("player_question").innerHTML = "Question Turn : " + player1_name;
+document.getElementById("player_answer").innerHTML = "Answer Turn : " + player2_name;
 
 function send() 
 {
@@ -25,4 +25,80 @@ function send()
     document.getElementById("output").innerHTML = row;
     document.getElementById("number_1").value = "";
     document.getElementById("number_2").value = "";
+}
+
+question_turn = "player1";
+Answer_turn = "player2";
+
+function check()
+{
+    get_answer = document.getElementById("input_check_box").value;
+    answer = get_answer.toLowerCase();
+    console.log("answer in lowercase= " + answer);
+
+    if(answer == actual_answer)
+    {
+        if(Answer_turn == "player1")
+        {
+            player1_score = player1_score + 1;
+            update_score();
+            document.getElementById("player1_score").innerHTML = player1_score;
+
+        }
+        else
+        {
+            player2_score = player2_score + 1;
+            update_score();
+            document.getElementById("player2_score").innerHTML = player2_score;
+        }
+
+    }
+    else
+    {
+       if(Answer_turn == "player1")
+       {
+          player1_score = player1_score - 1;
+          player2_score = player2_score + 1;
+          update_score();
+          document.getElementById("player2_score").innerHTML = player2_score;
+          document.getElementById("player1_score").innerHTML = player1_score;
+       }
+       else{
+        player1_score = player1_score + 1;
+        player2_score = player2_score - 1;
+        update_score();
+        document.getElementById("player2_score").innerHTML = player2_score;
+        document.getElementById("player1_score").innerHTML = player1_score;
+       }
+    }
+
+    if(question_turn == "player1")
+    {
+       question_turn = "player2";
+       document.getElementById("player_question").innerHTML = "Question Turn : " + player2_name;
+    }
+    else
+    {
+        question_turn = "player1";
+        document.getElementById("player_question").innerHTML = "Question Turn : " + player1_name;
+    }
+
+    if(Answer_turn == "player1")
+    {
+        Answer_turn = "player2";
+        document.getElementById("player_answer").innerHTML = "Answer Turn : " + player2_name;
+    }
+    else
+    {
+        Answer_turn = "player1";
+        document.getElementById("player_answer").innerHTML = "Answer Turn : " + player1_name;
+    }
+    document.getElementById("output").innerHTML = "";
+}
+
+function update_score()
+{
+    localStorage.setItem("player1_score",player1_score);
+    localStorage.setItem("player2_score",player2_score);
+
 }
